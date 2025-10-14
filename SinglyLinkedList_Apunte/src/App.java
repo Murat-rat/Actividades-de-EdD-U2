9cc7582d8e369b35b3a0244d0d02a3e90fe0ed1d
Murat-rat <20243ds044@utez.edu.mx>
2025-10-14 15:27:17 -0600

    Apuntes de Singly Linked List

---
+++ SinglyLinkedList_Apunte/src/App.java
@@ -0,0 +1,17 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        SinglyLinkedList list = new SinglyLinkedList();
+        list.print();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        
+        list.print();
+
+        list.remove(2);
+        list.print();
+
+        System.out.println(list.contains(1));
+    }
+}
