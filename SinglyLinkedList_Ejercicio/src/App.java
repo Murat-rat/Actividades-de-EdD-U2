9eec010e076c80e17f1aceb71c82dd2e3123030c
Sundown-uwu <20243ds148@utez.edu.mx>
2025-10-15 08:42:51 -0600

    Ejercicio de SinglyLinkedList más limpio

--- SinglyLinkedList_Ejercicio/src/App.java
+++ SinglyLinkedList_Ejercicio/src/App.java
@@ -1,30 +1,16 @@
 public class App {
     public static void main(String[] args) throws Exception {
         SinglyLinkedList list = new SinglyLinkedList();
-        list.print();
-        list.add(1);
         list.add(1);
         list.add(1);
         list.add(2);
-        list.add(4);
-        list.add(4);
-        list.add(4);
-        list.add(4);
-        list.add(5);
-        list.add(5);
-        list.add(6);
-        list.add(7);
-        
-        list.print();
+        list.add(3);
+        list.add(3);
+        list.add(3);
 
-        list.remove(2);
-        list.print();
-
-        System.out.println(list.contains(1));
-        System.out.println("--------------------------");
         System.out.println(list.count(1));
         System.out.println(list.count(3));
-        System.out.println(list.count(4));
-        System.out.println(list.count(7));
+        System.out.println("----------------------");
+        list.print();
     }
 }
